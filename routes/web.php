c47d77d49b1ae94eca041db1a8bb8ca8755d177a
Henry Kalvi Kaalma <Dudetitan@gmail.com>
2025-03-11 19:06:13 +0200

    Done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\WeatherController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/weather', [WeatherController::class, 'getWeather']);
